9cf05bc626e38eb693e076fbd03f77618f5fd54f
Julaxe <julaxe8@gmail.com>
2022-06-26 11:30:07 -0400

    animation fixed

--- PlatformerGame/Assets/Scripts/Player/PlayerAnimator.cs
+++ PlatformerGame/Assets/Scripts/Player/PlayerAnimator.cs
@@ -5,21 +5,20 @@ namespace DefaultNamespace
 {
     public class PlayerAnimator : MonoBehaviour
     {
-        private Rigidbody2D _rigidbody;
-        private Animator _animator;
-        private JumpPlayer _jumpPlayer;
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private JumpPlayer _jumpPlayer;
         private static readonly int GroundedHash = Animator.StringToHash("Grounded");
         private static readonly int JumpHash = Animator.StringToHash("Jump");
         private static readonly int AirSpeedYHash = Animator.StringToHash("AirSpeedY");
         private static readonly int RunningHash = Animator.StringToHash("Running");
 
-        private void Awake()
+        private void OnValidate()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
             _animator = GetComponent<Animator>();
             _jumpPlayer = GetComponent<JumpPlayer>();
-            
-            
+     
             _jumpPlayer.jumpEvent += OnJumpEvent;
         }
 
